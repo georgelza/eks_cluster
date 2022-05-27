aad3a6f492b76e0edf7fe568651b57a370bcbd4f
George Leonard <georgelza@gmail.com>
2022-05-27 21:23:05 +0200

    check

--- main.tf
+++ main.tf
@@ -1,5 +1,18 @@
 # https://github.com/aws-ia/terraform-aws-eks-blueprints
 
+
+provider "aws" {
+  version                  = ">= 3.72"
+  region                   = var.region
+  profile                  = var.profile
+  shared_credentials_files = [var.shared_credentials_files]
+
+}
+
+
+data "aws_availability_zones" "available" {}
+
+
 terraform {
   required_version = ">= 1.0.0"
 
@@ -19,42 +32,6 @@ terraform {
   }
 }
 
-provider "aws" {
-  version                  = ">= 3.72"
-  region                   = var.region
-  profile                  = var.profile
-  shared_credentials_files = [var.shared_credentials_files]
-
-}
-
-
-provider "kubernetes" {
-  host                   = module.eks_blueprints.eks_cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
-    command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", module.eks_blueprints.eks_cluster_id]
-  }
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = module.eks_blueprints.eks_cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
-
-    exec {
-      api_version = "client.authentication.k8s.io/v1alpha1"
-      command     = "aws"
-      # This requires the awscli to be installed locally where Terraform is executed
-      args = ["eks", "get-token", "--cluster-name", module.eks_blueprints.eks_cluster_id]
-    }
-  }
-}
-
-data "aws_availability_zones" "available" {}
 
 locals {
   environment = var.environment # Environment area eg., preprod or prod
@@ -62,18 +39,20 @@ locals {
   region      = var.region
   profile     = var.profile
 
-  vpc_cidr     = var.vpc_cidr
-  vpc_name     = var.vpc_name
-  azs          = var.azs
-  cluster_name = var.cluster_name
+  vpc_cidr      = var.vpc_cidr
+  vpc_name      = var.vpc_name
+  azs           = var.azs
+  cluster_name  = var.cluster_name
+  name          = basename(path.cwd)
 
   #terraform_version = "Terraform v1.0.1"
 
   # Add merge statement into the various resources to use this + whats required locally.
   tags = {
-    team    = var.team,
-    project = var.project,
+    team        = var.team,
+    project     = var.project,
+    Blueprint   = local.name
+    GithubRepo  = "github.com/aws-ia/terraform-aws-eks-blueprints"
   }
 
 }
-
